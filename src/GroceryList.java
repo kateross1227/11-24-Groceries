f95b3ccf313956576e780bea07f0e554166c1381
Kate Ross <kateross1227@gmail.com>
2014-11-25 12:06:32 -0700

    Update GroceryList.java

--- src/GroceryList.java
+++ src/GroceryList.java
@@ -1,1 +1,30 @@
 // class to store list of Grocery items 
+// class to store list of Grocery items 
+import java.util.*;
+
+public class GroceryList {
+  ArrayList <GroceryItem> list;
+  
+  public GroceryList(){
+    list = new ArrayList <GroceryItem>();
+  }
+  
+  public void add(GroceryItem item) {
+    list.add(item);
+  }
+  
+  public double getTotalCost() {
+    int sum = 0;
+    for (int i = 0; i < list.size(); i++) {
+      sum += list.get(i).getCost();
+    }
+    return sum;
+  }
+  public String toString() {
+    String s = "";
+    for (int i = 0; i < list.size(); i++) {
+      s += list.get(i).toString() + "\n";
+    }
+    return s;
+  }
+}
